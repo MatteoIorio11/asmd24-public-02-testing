c0d91d436dc75bd27f795a53b1cef2de101878b9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-01 09:52:07 +0100

    feat: add new unit test for checking the collaboration among the different components

--- src/test/java/integration/DeviceWithPolicyTest.java
+++ src/test/java/integration/DeviceWithPolicyTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,5 +36,25 @@ public class DeviceWithPolicyTest {
         this.device.on();
         assertTrue(this.device.isOn());
         verify(this.mockFailingPolicy).attemptOn();
+    }
+
+    @Description("When a new device is reset after being turned on, also the FailingPolicy should be reset")
+    @Tag("integration")
+    @Test
+    public void testCollaborationDuringReset() {
+        when(this.mockFailingPolicy.attemptOn()).thenReturn(true);
+        this.device.on();
+        this.device.reset();
+        verify(this.mockFailingPolicy).reset();
+        assertFalse(this.device.isOn());
+    }
+
+    @Description("When the dumping the Device as a String, the policy name should be included")
+    @Tag("integration")
+    @Test
+    public void testDeviceToString() {
+        when(this.mockFailingPolicy.policyName()).thenReturn("mock");
+        assertTrue(this.device.toString().contains("mock"));
+        verify(this.mockFailingPolicy).policyName();
     }
 }
