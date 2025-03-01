923e205c0030ccce6f2cc161acddb8bc22da6e47
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-01 09:32:10 +0100

    feat: add unit test for checking the random policy 'attemptOn'

--- src/test/java/unit/policy/RandomUnitTest.java
+++ src/test/java/unit/policy/RandomUnitTest.java
@@ -6,19 +6,47 @@ import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+import static org.mockito.Mockito.when;
 
 public class RandomUnitTest {
-    private FailingPolicy failingPolicy;
+    FailingPolicy failingPolicy;
+    @Mock RandomGenerator fixedRandom;
+    private static final String RANDOM_ATTRIBUTE_NAME = "random";
 
     @BeforeEach
     public void unit() {
-        this.failingPolicy = new RandomFailing();
+        try {
+            MockitoAnnotations.openMocks(this);  // This initializes @Mock fields
+            this.failingPolicy = new RandomFailing();
+            final Field field = this.failingPolicy.getClass().getDeclaredField(RANDOM_ATTRIBUTE_NAME);
+            field.setAccessible(true);
+            field.set(this.failingPolicy, this.fixedRandom);
+        } catch (Exception e) {
+            e.printStackTrace();  // Always print exceptions to debug issues
+        }
     }
+
 
     @Description("The policy name of a RandomFailing policy should be 'random'")
     @Tag("unit")
     @Test
     public void testPolicyName() {
         assert(this.failingPolicy.policyName().equals("random"));
+    }
+
+    @Description("The RandomFailing policy should be able to be turned on")
+    @Tag("unit")
+    @Test
+    public void testAttemptOn() {
+        when(this.fixedRandom.nextBoolean()).thenReturn(false);
+        assert(this.failingPolicy.attemptOn());
     }
 }
