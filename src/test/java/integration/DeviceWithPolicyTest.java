5a44a484c8f18e009a081f65aa851c6b98dcf281
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-01 09:45:08 +0100

    feat: add unit test for checking if attemptOn is called during device 'on'

--- src/test/java/integration/DeviceWithPolicyTest.java
+++ src/test/java/integration/DeviceWithPolicyTest.java
@@ -3,12 +3,21 @@ package integration;
 import devices.Device;
 import devices.FailingPolicy;
 import devices.StandardDevice;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class DeviceWithPolicyTest {
-    private Device device;
+    @Spy
+    Device device;
     @Mock
     FailingPolicy mockFailingPolicy;
 
@@ -16,5 +25,15 @@ public class DeviceWithPolicyTest {
     public void init() {
         MockitoAnnotations.openMocks(this);
         this.device = new StandardDevice(this.mockFailingPolicy);
+    }
+
+    @Description("When a new device is turned on, the FailingPolicy should be called")
+    @Tag("integration")
+    @Test
+    public void testCollaborationDuringTurnOn() {
+        when(this.mockFailingPolicy.attemptOn()).thenReturn(true);
+        this.device.on();
+        assertTrue(this.device.isOn());
+        verify(this.mockFailingPolicy).attemptOn();
     }
 }
